1f49ab66e7920c6ca1c6c79aa7784a90c1648b56
Nick <nickpine@gmail.com>
2022-03-11 22:02:52 -0500

    React router Parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import Contact from './ContactComponent';
 import { COMMENTS } from '../shared/comments';
 import { PARTNERS } from '../shared/partners';
 import { PROMOTIONS } from '../shared/promotions';
+import CampsiteInfo from './CampsiteInfoComponent';
 
 class Main extends Component {
 
@@ -34,10 +35,20 @@ class Main extends Component {
             );
         };
 
+        const CampsiteWithId = ({ match }) => {
+            return (
+                <CampsiteInfo
+                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                />
+            );
+        };
+
         return (
             <div>
                 <Header />
                 <Switch>
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/contactus' component={Contact} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
